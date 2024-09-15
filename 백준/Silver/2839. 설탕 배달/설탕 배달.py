ecf75c16df385b2ebc08bb7039cea66dd5e7d343
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-09-15 17:00:19 +0900

    [Silver IV] Title: 설탕 배달, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -2,20 +2,25 @@ n = int(input())
 
 result = -1
 
+# 1. 5로 나누어 떨어지는 경우
 if n % 5 == 0 :
     result = n // 5
+
 
 else : 
     count = 0
     while n > 0:
+        # n이 0보다 클 동안 반복해서 3을 빼준다
         n -= 3
         count += 1
 
+        # 2. 숫자가 3과 5를 조합해서 나올 수 있는 경우
         if n % 5 == 0:
             count += n // 5
             result = count
             break
 
+        # 3. 3으로 나누어 떨어지는 경우
         elif n == 0 :
             result = count
             break
